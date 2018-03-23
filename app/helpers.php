fbd576c32f5a42d6b2c8b3522e3261060b4a5886
Rodolfo Ruiz <rrpadilla@gmail.com>
2018-03-23 13:17:53 +0000

    update trustedproxy_config function

--- app/helpers.php
+++ app/helpers.php
@@ -27,8 +27,13 @@ if (! function_exists('trustedproxy_config')) {
     function trustedproxy_config($key, $env_value)
     {
         if ($key === 'proxies') {
+            if ($env_value === '*' || $env_value === '**') {
+                return $env_value;
+            }
+
             return $env_value ? explode(',', $env_value) : null;
-        } elseif ($key === 'headers') {
+        } 
+        elseif ($key === 'headers') {
             if ($env_value === 'HEADER_X_FORWARDED_AWS_ELB') {
                 return Request::HEADER_X_FORWARDED_AWS_ELB;
             } elseif($env_value === 'HEADER_FORWARDED') {
